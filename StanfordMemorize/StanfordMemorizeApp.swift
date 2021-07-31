2aaf293a826490a36a3200a241acf859aaa391f6
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-08-01 00:29:34 +1200

    Initial Commit

---
+++ StanfordMemorize/StanfordMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordMemorizeApp.swift
+//  StanfordMemorize
+//
+//  Created by Paulo Orquillo on 1/08/21.
+//
+
+import SwiftUI
+
+@main
+struct StanfordMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
